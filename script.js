0588b4317ac56e76927ecc8faa56e164c43216db
941390 <109982323+941390@users.noreply.github.com>
2022-08-24 19:30:03 +0300

    تم حل الكلاس ورك

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // أنشئ المصفوفة تحت هذا الكومنت
 
+let orders = [];
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +9,29 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrdern = {name,id,phone,order}
+
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrdern )
+
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
 }
 
 function load() {
   let container = document.getElementById("container");
+  container.innerHTML = "";
   
   // طبّق الخطوة 6 تحت هذا الكومنت
+orders.forEach(item => {
+  container.innerHTML +=     `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
+});
+}
 
+
+
+function removeitm(){
+
+  const element = document.getElementById("container")
+  element.remove();
 }
